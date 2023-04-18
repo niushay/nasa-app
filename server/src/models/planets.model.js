87543bb4b972a08c2be3f00e83800e3d744cfde0
niushayousefi <niusha.mech@gmail.com>
2023-04-18 20:53:07 +0300

    Loading Data from CSV file

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -9,25 +9,29 @@ function isHabitablePlanet(planet) {
     && planet['koi_prad'] < 1.6;
 }
 
-fs.createReadStream('kepler_data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
-  .on('data', (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on('error', (err) => {
-    console.log(err);
-  })
-  .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
-    console.log(`${habitablePlanets.length} habitable planets found!`);
-  });
+function loadPlanetData() {
+    fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+      comment: '#',
+      columns: true,
+    }))
+    .on('data', (data) => {
+      if (isHabitablePlanet(data)) {
+        habitablePlanets.push(data);
+      }
+    })
+    .on('error', (err) => {
+      console.log(err);
+    })
+    .on('end', () => {
+      console.log(habitablePlanets.map((planet) => {
+        return planet['kepler_name'];
+      }));
+      console.log(`${habitablePlanets.length} habitable planets found!`);
+    });
+}
+
+
 
   module.exports = {
     planets : habitablePlanets
