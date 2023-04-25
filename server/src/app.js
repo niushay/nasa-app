f00386e7a3c2f5f2780e05660b782e9920419c29
niushayousefi <niusha.mech@gmail.com>
2023-04-25 12:14:46 +0300

    Serving client and server on the same port

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
+const path = require("path")
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetRouter = require("./routes/planets/planets.router");
 
@@ -8,7 +10,15 @@ const app = express();
 app.use(cors({
     'origin': "http://localhost:3000"
 }))
+app.use(morgan("combined"));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
+
 app.use(planetRouter);
 
 module.exports = app;
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,7 +1,7 @@
 const {planets} = require("./../../models/planets.model")
 
 function getAllPlanets(req, res){
-    console.log(planets);
+    // console.log(planets);
     return res.status(200).json(planets);
 }
 
